a0c9051ba5fc951f307cb7a6387260004b3a937c
Eugen <119263405+enbt0909@users.noreply.github.com>
2023-06-18 03:04:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-// const names = [
-//   "Andreas",
-//   "Aryani",
-//   "Daniel",
-//   "Expo",
-//   "Eugen",
-//   "Genti",
-//   "Hoanglong",
-//   "Isla",
-//   "Jacqueline",
-//   "Johann",
-//   "Maciej",
-//   "Thomas",
-//   "Michael",
-//   "Neda",
-//   "Niclas",
-//   "Stephan",
-//   "Sven"
-// ];
-// function generateRandomName() {
-//   let drumSound = document.getElementById("drumSound");
-//   drumSound.volume = 0.1;
-//   console.log(drumSound);
-//   drumSound.play();
-//   setTimeout(function () {
-//     const randomIndex = Math.floor(Math.random() * names.length);
-//     const randomName = names[randomIndex];
-//     document.getElementById("randomName").textContent = randomName;
-//   }, 4500);
-// }
 const names = [
   "Andreas",
   "Aryani",
